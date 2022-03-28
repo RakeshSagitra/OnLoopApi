ebc8b6addfcb72e4b9ea294db8678e48e4e4a344
Rakesh <sdevr09@gmail.com>
2022-03-28 16:34:33 +0530

    Feature: added nodemon and babel for es6 syntax

---
+++ bin/onLoopApi.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node --debug --trace-warnings
+
+/**
+ * Module dependencies.
+ */
+ require('@babel/register')({
+  extensions: ['.js', '.jsx', '.ts', '.tsx']
+})
+require('../main')
